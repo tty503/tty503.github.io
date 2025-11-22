9b82584f36a4be41ec8d196eef7236f14b740ae8
tty503 <sirhcqzram@gmail.com>
2025-11-22 13:44:24 -0400

    update

--- restore.sh
+++ restore.sh
@@ -1,1 +1,6 @@
-git clone https://github.com/tty503/tty503.github.io.git public
+cd public
+git remote -v 
+git remote set-url origin https://github.com/tty503/tty503.github.io.git
+git add .
+git commit -m "update" 
+git push origin main --force
